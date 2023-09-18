c4a41f876bed2138ff081fa571378930585bbb47
Vadim Cherepanov <xenos26@gmail.com>
2023-09-19 03:24:51 +0700

    feat: add alias for wget prettier

--- fish_config/config.fish
+++ fish_config/config.fish
@@ -1,9 +1,16 @@
 if status is-interactive
 alias ncal="ncal -b"
 alias shtd="sudo shutdown now"
-export EDITOR=/usr/bin/nvim
+alias dwprettier ="wget https://github.com/Rcf47/JavaScript/blob/main/experiments/prettier_Eslint/.prettierrc.json"
+alias dwprettierignore ="wget https://github.com/Rcf47/JavaScript/blob/main/experiments/prettier_Eslint/.prettierignore"
+export EDITOR=/snap/bin/nvim
 #alt+f for exact find
 bind \ef ~/Programms/scripts/findWithFzf.sh
 #alt+d just find
 bind \ed ~/Programms/scripts/findWithFzfnotExact.sh
+zoxide init fish | source
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
