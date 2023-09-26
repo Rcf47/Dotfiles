e65dd484429adcc122f2e8838ffc950762106ea3
Vadim Cherepanov <xenos26@gmail.com>
2023-09-27 06:50:35 +0700

    chore: add alias for copy prettier

--- fish_config/config.fish
+++ fish_config/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
 alias ncal="ncal -b"
 alias shtd="sudo shutdown now"
+alias cpprettier="cp ~/Education/JavaScript/experiments/prettier_Eslint/.prettierrc.json ~/Education/JavaScript/experiments/prettier_Eslint/.prettierignore ./"
 export EDITOR=/snap/bin/nvim
 #alt+f for exact find
 bind \ef ~/Programms/scripts/findWithFzf.sh
