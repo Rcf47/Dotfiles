262c9e71bcc9d2d3daf134f677506d6c100caf85
Rcf47 <xenos26@gmail.com>
2024-04-13 17:53:42 +0700

    fix: fix error with new alias

--- fish_config_arch/config.fish
+++ fish_config_arch/config.fish
@@ -1,19 +1,20 @@
 if status is-interactive
-  ### Aliases ###
-  # Navigation
-  alias .. = "cd .."
-  alias ... = "cd ../.."
-  alias .3 = "cd ../../.."
-  alias .4 = "cd ../../../.."
-  alias .5 = "cd ../../../../.."
-  alias cawesome="cd ~/.config/awesome"
-  alias cdwn="cd ~/Downloads"
-  alias ced="cd ~/Education/"
-  alias cnvim="cd ~/.config/nvim"
-  alias cgh="cd ~/Github"
+ ### Aliases ###
+ # Navigation
+ alias ..="cd .."
+ alias ...="cd ../.."
+ alias .3="cd ../../.."
+ alias .4="cd ../../../.."
+ alias .5="cd ../../../../.."
+ alias cawesome="cd ~/.config/awesome"
+ alias cdwn="cd ~/Downloads"
+ alias ced="cd ~/Education/"
+ alias cnvim="cd ~/.config/nvim"
+ alias cgh="cd ~/Github"
   # Others
  alias shtd="sudo shutdown now"   # Commands to run in interactive sessions can go here
  alias ls="eza -al --color=always --group-directories-first"
  export EDITOR=/usr/bin/nvim
+ export MANPAGER="nvim +Man!"
  fish_hybrid_key_bindings
 end
