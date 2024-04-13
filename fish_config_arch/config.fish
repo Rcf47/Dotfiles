93d5732e5fc5956452dd65bdd06f759445dec73e
Rcf47 <xenos26@gmail.com>
2024-04-13 16:46:47 +0700

    feat: add alias for base navigation in fish config

--- fish_config_arch/config.fish
+++ fish_config_arch/config.fish
@@ -1,11 +1,19 @@
 if status is-interactive
+  ### Aliases ###
+  # Navigation
+  alias .. = "cd .."
+  alias ... = "cd ../.."
+  alias .3 = "cd ../../.."
+  alias .4 = "cd ../../../.."
+  alias .5 = "cd ../../../../.."
+  alias cawesome="cd ~/.config/awesome"
+  alias cdwn="cd ~/Downloads"
+  alias ced="cd ~/Education/"
+  alias cnvim="cd ~/.config/nvim"
+  alias cgh="cd ~/Github"
+  # Others
  alias shtd="sudo shutdown now"   # Commands to run in interactive sessions can go here
  alias ls="eza -al --color=always --group-directories-first"
- alias cawesome="cd ~/.config/awesome"
- alias cdwn="cd ~/Downloads"
- alias ced="cd ~/Education/"
- alias cnvim="cd ~/.config/nvim"
- alias cgh="cd ~/Github"
  export EDITOR=/usr/bin/nvim
  fish_hybrid_key_bindings
 end
