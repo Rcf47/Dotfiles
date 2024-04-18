ef998119d39cf4c611616a1b7d9d9cef8dadd547
Rcf47 <xenos26@gmail.com>
2024-04-18 12:16:42 +0700

    feat: add new fish config

--- fish_config_arch/config.fish
+++ fish_config_arch/config.fish
@@ -13,8 +13,15 @@ if status is-interactive
  alias cgh="cd ~/Github"
   # Others
  alias shtd="sudo shutdown now"   # Commands to run in interactive sessions can go here
- alias ls="eza -al --color=always --group-directories-first"
+ alias ls="eza -al --color=always --group-directories-first --icons=always --no-user"
  export EDITOR=/usr/bin/nvim
  export MANPAGER="nvim +Man!"
+ export HISTSIZE=10000
  fish_hybrid_key_bindings
+
+# Set up fzf key bindings
+ fzf --fish | source
+
+ #turn off the screen saver
+ xset s off -dpms
 end
