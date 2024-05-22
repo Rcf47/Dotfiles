d3e860143a51c6265cbf908885f3dab292afd0b3
Vadim Cherepanov <xenos26@gmail.com>
2024-05-22 16:26:09 +0700

    feat: add script helpers for arch install

---
+++ ForArchInstall/install_from_yay.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -S google-chrome pomatez ttf-meslo-nerd-font-powerlevel10k timer xkblayout xkb-switch ttf-material-design-icons-git prettierd nomacs rofi-greenclip
---
+++ ForArchInstall/makedir.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir ~/Github/ ~/Downloads/ ~/Education/ ~/Backups/ ~/.sessions/ ~/other ~/Programms ~/Pictures
+
+mkdir -p ~/.config/awesome ~/.config/picom ~/.config/nvim ~/.config/rofi ~/.config/tmux ~/.config/eslint_d ~/.config/prettierd ~/.config/stylua
+
+mkdir -p ~/.config/bat/themes
